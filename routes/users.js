958ce752e9288008d9cdb3c4b2dc3fe26142ca89
HichamBENKADA <hicham20014@gmail.com>
2024-01-28 08:14:36 -0500

    routes are added

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Get all users
+router.get('/', userController.getAllUsers);
+
+// Create a new user
+router.post('/', userController.createUser);
+
+// Get users by id
+router.get('/:id', userController.getUserById);
+
+// Update a user
+router.patch('/:id', userController.updateUser);
+
+// Delete a user
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-const express =require('express');
-require()
-//Server app
-const app =express();
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+//Routes
+const connectDB = require('./confid/db');
+const posts = require('./routes/posts');
+const users = require('./routes/users');
+const comments = require('./routes/comments');
+
+
+// Connect to the database
+connectDB();  
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use the routes
+app.use('/api/posts', posts);
+app.use('/api/users', users);
+app.use('/api/comments', comments);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
